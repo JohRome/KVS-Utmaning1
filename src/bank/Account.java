f105764dacfee700fb08ed25afcd0d49f38dce78
Johan Romeo <jofiluren@outlook.com>
2023-05-23 04:54:39 +0200

    Satte grunden till projektet

---
+++ src/bank/Account.java
@@ -0,0 +1,26 @@
+package bank;
+
+public class Account {
+
+    private double balance;
+    private int accountNumber;
+
+    public Account(double initialCash, int accountNumber) {
+
+    }
+
+    public double deposit(double cash) {
+        return 0;
+    }
+    public double withdraw(double cash) {
+        return 0;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+}
