f099d72b98b25898086eb3366166c18b18a1ef66
Johan Romeo <jofiluren@outlook.com>
2023-05-23 05:42:28 +0200

    initial cash constructor - KLAR

--- src/bank/Account.java
+++ src/bank/Account.java
@@ -5,17 +5,26 @@ package bank;
 
 public class Account {
 
-    private double balance;
+    private final double balance;
     private int accountNumber;
 
     /**Sets initial balance and account number*/
     public Account(double initialCash, int accountNumber) {
         // Can't be able to add a negative amount of cash
+        if (initialCash < 0) {
+            System.out.println("Balance can't be < 0");
+            this.balance = 0;
+        } else {
+            this.balance = initialCash;
+        }
+
+        // Can't be able to add a negative account number
     }
 
     /**Add money to balance*/
     public double deposit(double cash) {
         // Can't be able to add a negative amount of cash
+        // Can't be able to deposit 0
         return 0;
     }
     /**Withdraw money from balance*/
--- tests/bank/AccountTest.java
+++ tests/bank/AccountTest.java
@@ -1,7 +1,19 @@
 package bank;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
 
+    Account sut;
+    @Test
+    public void cantSetInitialCashToNegativeNumber() {
+        // Arrange
+        double expected = 0;
+        // Act
+        sut = new Account(-1, 0);
+        // Assert
+        assertEquals(expected, sut.getBalance());
+    }
 }
